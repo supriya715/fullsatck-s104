f8b6a2002878989d6af4b449f65b53677b6ee6c3
supriya715 <163844156+supriya715@users.noreply.github.com>
2025-09-06 21:01:42 +0530

    Update vite.config.js

--- crud_front/vite.config.js
+++ crud_front/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/frontend/',  // important for Tomcat context path
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: './', // important for Tomcat deployment
+  build: {
+    outDir: 'dist',
+  }
+});
 
